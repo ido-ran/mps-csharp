b7779450984c472fb9f6e343fa6776470ebd737a
rhencke <rhencke@vestcom.com>
2009-07-13 04:26:53 +0000

    namespace generator support

--- solutions/CSharp3.sandbox/source_gen/CSharp3/sandbox/sandbox/My Compilation Unit.cs
+++ solutions/CSharp3.sandbox/source_gen/CSharp3/sandbox/sandbox/My Compilation Unit.cs
@@ -1,4 +1,16 @@
 // Auto generated - do not edit.
 extern alias joe;
 extern alias schmoe;
-using System.;
+using Bar;
+namespace Bar
+{
+  extern alias foo;
+  extern alias bar;
+  namespace Foo
+  {
+    namespace Baz
+    {
+      extern alias abc;
+    }
+  }
+}
